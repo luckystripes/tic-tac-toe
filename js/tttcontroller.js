5425bd7c7911f253e0bd36b33c4ae7e0ece1149e
Nancy Ma <luckystripesbiz@gmail.com>
2015-04-03 14:15:23 -0700

    updated css. changed to cheech vs chong

--- js/tttcontroller.js
+++ js/tttcontroller.js
@@ -2,119 +2,142 @@ angular
 	.module("tictactoe")
 	.controller("tttcontroller", tttcontroller);
 
-	tttcontroller.$inject = ['$firebaseArray'];
+	tttcontroller.$inject = ['$firebaseObject'];
 
-function tttcontroller ($firebaseArray){
+function tttcontroller ($firebaseObject){
 	var self = this;
 	
 	// players
-	var x = "ios";
-	var o = "android";
-	var currentPlayer = x;
-	var blank = "";
+	var x = "cheech";
+	var o = "chong";
+	// var currentPlayer = x;
+	var moves = 0;
 	
 	// functions
+	self.game = syncFiyah();
 	self.playermoves = playermoves;
 	self.winner = winner;
 	self.tied = tied;
+	
 
-
-	var moves = 0;
 
 	// board array
-	self.board=[
-		{value:""},
-		{value:""},
-		{value:""},
-		{value:""},
-		{value:""},
-		{value:""},
-		{value:""},
-		{value:""},
-		{value:""}
-	]
+	// self.board=[
+	// 	{value:""},
+	// 	{value:""},
+	// 	{value:""},
+	// 	{value:""},
+	// 	{value:""},
+	// 	{value:""},
+	// 	{value:""},
+	// 	{value:""},
+	// 	{value:""}
+	// ]
 
 /////////////////////////////////////////FUNCTIONS///////////////////////////
 
+// sync game with firebase
+	function syncFiyah (){
+		var ref = new Firebase("tic-tac-toe-what.firebaseIO.com/board");
+		var gameObj = $firebaseObject(ref);
+
+		gameObj.$loaded(function(){
+			gameObj.x = "cheech";
+			gameObj.o = "chong";
+			gameObj.currentPlayer = x;
+			gameObj.moves = 0;
+
+			gameObj.board=[
+					{value:"", image:"img/blank.gif"},
+					{value:"", image:"img/blank.gif"},
+					{value:"", image:"img/blank.gif"},
+					{value:"", image:"img/blank.gif"},
+					{value:"", image:"img/blank.gif"},
+					{value:"", image:"img/blank.gif"},
+					{value:"", image:"img/blank.gif"},
+					{value:"", image:"img/blank.gif"},
+					{value:"", image:"img/blank.gif"}
+			]
+
+			gameObj.$save();
+		}) //end of $loaded
+
+		return gameObj;
+	} //end of syncFiyah
+
+
 // alternates x and o moves
 	function playermoves ($index) {
-			 // var ref = new Firebase ("tic-tac-toe-what.firebaseIO.com/board")
-			 // var move = $firebaseObject(ref)
-		
-		
-		if (currentPlayer === x){
-			self.board[$index].image = "img/apple.png";
-			self.board[$index].value = "ios";	
-			winner(currentPlayer);
-			moves++;
-			currentPlayer = o;
-			tied(moves);	
+		if (self.game.currentPlayer === x){
+			self.game.board[$index].image = "img/cheech.png";
+			self.game.board[$index].value = "cheech";	
+			// winner(self.game.currentPlayer);
+			self.game.moves ++;
+			self.game.currentPlayer = o;
+			tied(moves);
+			self.game.$save();	
 		
 		} 	else {
-			self.board[$index].image = "img/android.png";
-			self.board[$index].value = "android";
-			currentPlayer = o;			
-			winner(currentPlayer);
-			moves++;
-			currentPlayer = x;
+			self.game.board[$index].image = "img/chong.png";
+			self.game.board[$index].value = "chong";
+			self.game.currentPlayer = o;			
+			// winner(self.game.currentPlayer);
+			self.game.moves++;
+			self.game.currentPlayer = x;
 			tied(moves);
+			self.game.$save();
 	
 		} 
 
 		// NEED TO PREVENT PLAYER FROM CHANGING MOVE	
-	}
-// end playermoves///////////////////////////////////////////////
+	}// end playermoves///////////////////////////////////////////////
 
 
 // get winner who has any 3 in a row 
 	function winner (currentPlayer){
-		console.log("winner function called");
-		console.log("asdasdf");
-			// horizontal wins ///////////////IOS//////////////////////////
-		if ( (self.board[0].value ==="ios") && (self.board[1].value ==="ios") && (self.board[2].value ==="ios")|| 
-			 (self.board[3].value ==="ios") && (self.board[4].value ==="ios") && (self.board[5].value ==="ios")||
-			 (self.board[6].value ==="ios") && (self.board[7].value ==="ios") && (self.board[8].value ==="ios")|| 
+			// horizontal wins ///////////////cheech//////////////////////////
+		if ( (self.game.board[0].value ==="cheech") && (self.game.board[1].value ==="cheech") && (self.game.board[2].value ==="cheech")|| 
+			 (self.game.board[3].value ==="cheech") && (self.game.board[4].value ==="cheech") && (self.game.board[5].value ==="cheech")||
+			 (self.game.board[6].value ==="cheech") && (self.game.board[7].value ==="cheech") && (self.game.board[8].value ==="cheech")|| 
 			 // vertical wins
-			 (self.board[0].value ==="ios") && (self.board[3].value ==="ios") && (self.board[6].value ==="ios")||
-			 (self.board[1].value ==="ios") && (self.board[4].value ==="ios") && (self.board[7].value ==="ios")|| 
-			 (self.board[2].value ==="ios") && (self.board[5].value ==="ios") && (self.board[8].value ==="ios")||
+			 (self.game.board[0].value ==="cheech") && (self.game.board[3].value ==="cheech") && (self.game.board[6].value ==="cheech")||
+			 (self.game.board[1].value ==="cheech") && (self.game.board[4].value ==="cheech") && (self.game.board[7].value ==="cheech")|| 
+			 (self.game.board[2].value ==="cheech") && (self.game.board[5].value ==="cheech") && (self.game.board[8].value ==="cheech")||
 			 // diaganol wins
-			 (self.board[0].value ==="ios") && (self.board[4].value ==="ios") && (self.board[8].value ==="ios")|| 
-			 (self.board[2].value ==="ios") && (self.board[4].value ==="ios") && (self.board[6].value ==="ios") ){
+			 (self.game.board[0].value ==="cheech") && (self.game.board[4].value ==="cheech") && (self.game.board[8].value ==="cheech")|| 
+			 (self.game.board[2].value ==="cheech") && (self.game.board[4].value ==="cheech") && (self.game.board[6].value ==="cheech") ){
 
-			alert(currentPlayer + "wins");
+			alert("cheech gets the hit");
 			
-			// horizontal wins /////////ANDROID////////////////////////////
-		} else if ( (self.board[0].value === "android") && (self.board[1].value ==="android") && (self.board[2].value==="android")|| 
-					(self.board[3].value === "android") && (self.board[4].value ==="android") && (self.board[5].value==="android")||
-					(self.board[6].value === "android") && (self.board[7].value ==="android") && (self.board[8].value==="android")|| 
+			// horizontal wins /////////chong////////////////////////////
+		} else if ( (self.game.board[0].value === "chong") && (self.game.board[1].value ==="chong") && (self.game.board[2].value==="chong")|| 
+					(self.game.board[3].value === "chong") && (self.game.board[4].value ==="chong") && (self.game.board[5].value==="chong")||
+					(self.game.board[6].value === "chong") && (self.game.board[7].value ==="chong") && (self.game.board[8].value==="chong")|| 
 					 // vertical wins
-					(self.board[0].value === "android") && (self.board[3].value ==="android") && (self.board[6].value==="android")||
-					(self.board[1].value === "android") && (self.board[4].value ==="android") && (self.board[7].value==="android")|| 
-					(self.board[2].value === "android") && (self.board[5].value ==="android") && (self.board[8].value==="android")||
+					(self.game.board[0].value === "chong") && (self.game.board[3].value ==="chong") && (self.game.board[6].value==="chong")||
+					(self.game.board[1].value === "chong") && (self.game.board[4].value ==="chong") && (self.game.board[7].value==="chong")|| 
+					(self.game.board[2].value === "chong") && (self.game.board[5].value ==="chong") && (self.game.board[8].value==="chong")||
 					 // diaganol wins
-					(self.board[0].value === "android") && (self.board[4].value ==="android") && (self.board[8].value==="android")|| 
-					(self.board[2].value === "android") && (self.board[4].value ==="android") && (self.board[6].value==="android")){
+					(self.game.board[0].value === "chong") && (self.game.board[4].value ==="chong") && (self.game.board[8].value==="chong")|| 
+					(self.game.board[2].value === "chong") && (self.game.board[4].value ==="chong") && (self.game.board[6].value==="chong")){
 
-			alert(currentPlayer + "wins");
+			alert("chong gets the hit");
 			
 		}	else{
 			null;
 		}
 
 
-		}
-// end winner//////////////////////////////////////////////////
+		}// end winner//////////////////////////////////////////////////
 
 // tied
 	function tied (moves){
-		if (moves===9) {
+		if (self.game.moves===9) {
 
 			alert("tied");
 
 		};
-	}
-// end tied/////////////////////////////////////////////////////
+	}// end tied/////////////////////////////////////////////////////
 
 
 
